a865d0932ea3878cdf5465baf1f9d96b5b425391
mwleungac <mwleungac@oocl.com>
2020-04-21 15:12:23 +0800

    added onIncrease()

--- src/Component/Counter.js
+++ src/Component/Counter.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react'
 
 export default class Counter extends Component {
+constructor(props) {
+    super(props);
+
+    this.onIncrease = this.onIncrease.bind(this);
+    this.onDecrease = this.onDecrease.bind(this);
+    
+    this.state = {
+         number: 0
+    };
+}
+
+onIncrease(){
+    this.setState(prevState => {
+        return{
+        number: prevState.number + 1
+        };
+    })
+}
+
+onDecrease(){
+    this.setState(prevState => {
+        return{
+        number: prevState.number - 1
+        };
+    })
+}
+
+
     render() {
         return (
             <div>
-                <button>+</button>
-                <span>30</span>
-                <button>-</button>
+                <button onClick = {this.onIncrease}>+</button>
+                <span>{this.state.number}</span>
+                <button onClick = {this.onDecrease}>-</button>
             </div>
         )
     }
